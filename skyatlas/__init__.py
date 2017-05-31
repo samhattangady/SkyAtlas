de814e9ca564fa1d7be62cc314b59c7b2f8b0db2
Samarth Hattangady <samhattangady@gmail.com>
2017-05-31 11:53:42 +0530

    moved functions out of __init__

--- skyatlas/__init__.py
+++ skyatlas/__init__.py
@@ -24,25 +24,5 @@ Eventually there will be support to convert between different formats.
 The proposed formats are GeoJSON, KML+KMZ and shapefiles.
 """
 
-import json
-import os 
-
 from .classes import SkyAtlas
-
-def load_geojson(json_in):
-    # TODO Check what else are the possible inputs here
-    if os.path.isfile(json_in):
-        with open(json_in) as in_file:
-            geojson_data = json.load(in_file)
-    elif type(json_in) == str:
-        geojson_data = json.loads(json_in)
-    elif type(json_in) == dict:
-        geojson_data = json_in
-    else:
-        # TODO Throw exception
-        return None
-    return SkyAtlas().load_geojson(geojson_data)
-
-def load(input):
-    # TODO parse input, call corresponding load function
-    pass
+from .loaders import load_geojson
---
+++ skyatlas/loaders.py
@@ -0,0 +1,22 @@
+import json
+import os
+
+from .classes import SkyAtlas
+
+def load_geojson(json_in):
+    # TODO Check what else are the possible inputs here
+    if os.path.isfile(json_in):
+        with open(json_in) as in_file:
+            geojson_data = json.load(in_file)
+    elif type(json_in) == str:
+        geojson_data = json.loads(json_in)
+    elif type(json_in) == dict:
+        geojson_data = json_in
+    else:
+        # TODO Throw exception
+        return None
+    return SkyAtlas().load_geojson(geojson_data)
+
+def load(input):
+    # TODO parse input, call corresponding load function
+    pass
